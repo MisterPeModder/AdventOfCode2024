3d399f27983f2f2c14c410f134df38afaa9dca5b
Yanis Guaye <yguaye44@gmail.com>
2024-12-08 04:16:54 +0100

    runner - allow arbitrary solution types for solution functions

--- src/empty_main.zig
+++ src/empty_main.zig
@@ -12,6 +12,7 @@ pub const std_options = .{
 pub const AOC_YEAR: u32 = 2024;
 pub const AOC_DAY: u32 = 1;
 pub const AocData = struct { data: u32 };
+pub const AocResult = u32;
 
 pub fn aocSetup(allocator: Allocator, input: []const u8) anyerror!AocData {
     _ = allocator;
@@ -19,13 +20,13 @@ pub fn aocSetup(allocator: Allocator, input: []const u8) anyerror!AocData {
     return .{ .data = 0 };
 }
 
-pub fn aocPart1(allocator: Allocator, data: AocData) anyerror!u32 {
+pub fn aocPart1(allocator: Allocator, data: AocData) anyerror!AocResult {
     _ = allocator;
     _ = data;
     return 1;
 }
 
-pub fn aocPart2(allocator: Allocator, data: AocData) anyerror!u32 {
+pub fn aocPart2(allocator: Allocator, data: AocData) anyerror!AocResult {
     _ = allocator;
     _ = data;
     return 1;
